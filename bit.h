053e483b5ec8cdd5521a060d61e3128695b381a8
artem98 <temiko_oreshkin@bk.ru>
2020-12-15 10:14:52 -0800

    NEW: bit class

---
+++ bit.h
@@ -0,0 +1,75 @@
+class bit
+{
+private:
+  bool m_bit;
+public:
+  int getInt () const { return m_bit ? 1 : 0; }
+  bool get () const { return m_bit; }
+  void set (bool value) { m_bit = value; }
+  void inverse () { m_bit = !m_bit; }
+
+  bit (bool value = false) { m_bit = value; }
+
+  const bit operator+ (const bit& rhs) const {
+      return bit ((m_bit && !rhs.get ()) || (!m_bit && rhs.get ()));
+  }
+
+  const bit operator* (const bit& rhs) const {
+      return bit (m_bit && rhs.get () );
+  }
+
+  bit& operator= (const bit& rhs) {
+      if (this == &rhs) {
+          return *this;
+      }
+      m_bit = rhs.get ();
+      return *this;
+  }
+
+  friend bool operator> (const bit &lhs, const bit &rhs);
+  friend bool operator<= (const bit &lhs, const bit &rhs);
+
+  friend bool operator< (const bit &lhs, const bit &rhs);
+  friend bool operator>= (const bit &lhs, const bit &rhs);
+
+  friend bool operator== (const bit &lhs, const bit &rhs);
+  friend bool operator!= (const bit &lhs, const bit &rhs);
+
+};
+
+bit Not (bit& b)
+{
+  return bit (!b.get ());
+}
+
+int toInt (bit& b)
+{
+  return b.get () ? 1 : 0;
+}
+
+bool operator> (const bit &lhs, const bit &rhs)
+{
+  return lhs.m_bit && !rhs.m_bit;
+}
+bool operator<= (const bit &lhs, const bit &rhs)
+{
+  return (lhs < rhs) || (lhs == rhs);
+}
+
+bool operator< (const bit &lhs, const bit &rhs)
+{
+  return !lhs.m_bit && rhs.m_bit;
+}
+bool operator>= (const bit &lhs, const bit &rhs)
+{
+  return (lhs > rhs) || (lhs == rhs) ;
+}
+
+bool operator== (const bit &lhs, const bit &rhs)
+{
+  return (lhs.m_bit && rhs.m_bit) || (!lhs.m_bit && !rhs.m_bit);
+}
+bool operator!= (const bit &lhs, const bit &rhs)
+{
+  return (lhs.m_bit && !rhs.m_bit) || (!lhs.m_bit && rhs.m_bit);
+}
