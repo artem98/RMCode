021ac90e503335273bd67180f574f56d2edc933c
artem98 <temiko_oreshkin@bk.ru>
2020-12-18 13:52:15 -0800

    NEW: decoder: func correction + bit vote

--- decoder.cpp
+++ decoder.cpp
@@ -1,5 +1,31 @@
 #include "decoder.h"
 
+// f' = f - a * x_i1 * ... * x_is
+// a is supposed to be 1
+// x_i1 ... x_is is stored as:
+// 0, 0, 1, 0, 1, ... 1, 0
+// ......i1....i2.....is..
+void change_func_table (const bit* vars,
+                        const size_t vars_len,
+                        bit* table,
+                        const size_t table_len)
+{
+  auto is_any_chosen_variable_true = [&] (size_t line) ->bool {
+    for (size_t ind = 0; ind < vars_len; ind++)
+      {
+        if (vars[ind].get () && ((line >> ind) & 1))
+          return 1;
+      }
+    return false;
+  };
+
+  for (size_t line = 0; line < table_len; line++)
+    {
+      if (is_any_chosen_variable_true (line))
+        table[line].inverse ();
+    }
+}
+
 bit get_table_value_for_variables (const bit* table,
                                    const size_t table_len,
                                    const bit* vars, // from x_1 to x_n
@@ -15,4 +41,19 @@ bit get_table_value_for_variables (const bit* table,
   return table[index];
 }
 
+bit vote (const bit* values_for_vote, const size_t len)
+{
+  size_t zero_count = 0;
+  size_t one_count = 0;
 
+  for (size_t ind = 0; ind < len; ind++)
+    {
+      if (values_for_vote[ind].get ())
+        one_count++;
+      else
+        zero_count++;
+    }
+  if (one_count > zero_count)
+    return bit (true);
+  return bit (false);
+}
--- decoder.h
+++ decoder.h
@@ -3,9 +3,18 @@
 
 #include "bit.h"
 
+// f' = f - a * x_i1 * ... * x_is
+// a is supposed to be 1
+void change_func_table (const bit* vars,
+                        const size_t vars_len,
+                        bit* table,
+                        const size_t table_len);
+
 bit get_table_value_for_variables (const bit* table,
                                    const size_t table_len,
                                    const bit* vars, // from x_1 to x_n
                                    const size_t vars_len);
+
+bit vote (const bit* values_for_vote, const size_t len);
 
 #endif // DECODER_H
