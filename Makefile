1255aae3ac140e94ce3e055e55cf3ce6c195f41b
konstantin.kashin <ko-kashin@yandex.ru>
2020-12-19 11:01:28 +0300

    ENH: add decoder.o to compile list & fix build

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXXFLAGS := -g -Wall -W -Wextra -std=c++11 --fast-math -O3 -MMD
 LDLIBS := -lm -lstdc++
 
-main: main.o bit.o matrix.o encoder.o
+main: main.o bit.o matrix.o encoder.o decoder.o
 DEPS := $(shell find -name "*.d")
 -include $(DEPS)
 
--- decoder.cpp
+++ decoder.cpp
@@ -27,7 +27,7 @@ void change_func_table (const bit* vars,
 }
 
 bit get_table_value_for_variables (const bit* table,
-                                   const size_t table_len,
+                                   const size_t /*table_len*/,
                                    const bit* vars, // from x_1 to x_n
                                    const size_t vars_len)
 {
